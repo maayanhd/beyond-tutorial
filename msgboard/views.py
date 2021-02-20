b37d80fccdd2ec4e96e40688a9926606558cdb4b
Maayan Hadar <maayan.hadarwork@gmail.com>
2021-02-20 11:52:15 +0200

    Revert "Merge pull request #7 from maayanhd/view-functions-and-templates"
    This reverts commit e46ab08346ad5dbf96542d7858176958a689df35, reversing
    changes made to b32579dc9f7eddebaf6483d7f748c6b8861e69d8.

--- msgboard/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Message
-
-
-def board(request):
-    messages = Message.objects.order_by('-date')
-    return render(request, 'msgboard/board.html', {'messages': messages})
